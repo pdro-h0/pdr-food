03ce3e6e228f26ae2b3cbd00f92b117e178335ed
pdro-h0 <pedro.pho792@gmail.com>
2024-05-01 21:45:12 -0300

    style: improve logo quality on header

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -2,11 +2,16 @@ import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="logo da PDR Food" width={100} height={30} />
+      <div className="relative h-[30px] w-[100px]">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo da PDR Food" fill />
+        </Link>
+      </div>
 
       <Button
         size="icon"
